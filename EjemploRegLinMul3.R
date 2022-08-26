f67f515f288b7e6ccda53e426693139c3eae6a4d
velazquez@ciencias.unam.mx <velazquez@ciencias.unam.mx>
2022-08-26 16:38:59 -0500

    ejercicio de ancova

---
+++ EjemploRegLinMul3.R
@@ -0,0 +1,298 @@
+# Regresión lineal múltiple
+# Ajuste e interpretación
+# Variables categóricas
+
+rm(list = ls(all.names = TRUE))
+gc()
+
+setwd("/home/diego/Documents/Aprendizaje_Estadistica")
+Datos=read.csv("ejemplo3RLM.csv", header=TRUE )
+
+summary(Datos)
+str(Datos)
+
+#Siempre asegurarse que la variable categórica está definida como factor
+Datos$X1c=factor(Datos$X1c)
+str(Datos)
+
+par(mfrow=c(1,1),mar=c(4.5,4.5,1,1))
+plot(Datos)
+library(GGally)
+ggpairs(Datos)
+
+ggpairs(data=Datos, title="Datos", aes(colour = X1c))
+
+X11()
+with(Datos, plot(X2, y, col=c("red", "brown", "blue")[Datos[,c("X1c")]] ))
+legend("topleft",levels(Datos[,c("X1c")]), col=c("red", "brown", "blue"), pch = c(0,0,0),  pt.cex=1.5,cex = .9, y.intersp = 1.3 , bty="n")
+
+
+
+# R siempre considera como nivel de referencia el primer nivel
+# guardado en el metadato de etiquetas
+levels(Datos$X1c)
+# El modelo que se ajusta por default en R 
+# incluyendo s?lo las dos variables X1c y X2 es
+# E(y;x)=b0+b1A2+b2A3+b3x2
+
+fit=lm(y~X1c+X2, data=Datos)
+summary(fit)
+'''
+Call:
+lm(formula = y ~ X1c + X2, data = Datos)
+
+Residuals:
+     Min       1Q   Median       3Q      Max 
+-0.27765 -0.06051  0.00333  0.06529  0.26148 
+
+Coefficients:
+            Estimate Std. Error t value Pr(>|t|)    
+(Intercept)  0.50534    0.01385    36.5   <2e-16 ***
+X1cA2        1.31309    0.01405    93.4   <2e-16 ***
+X1cA3        0.49956    0.01400    35.7   <2e-16 ***
+X2           1.49718    0.00304   491.8   <2e-16 ***
+---
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+Residual standard error: 0.099 on 296 degrees of freedom
+Multiple R-squared:  0.999,	Adjusted R-squared:  0.999
+
+# Al menos una es distinta de cero 
+F-statistic: 8.16e+04 on 3 and 296 DF,  p-value: <2e-16
+'''
+
+
+#Analicemos la Interpretación del modelo con estos parámetros. 
+#Después regresaremos a la prueba asociada a la tabla ANOVA.
+#Aqu? E(y;x1c, x2)=b0+b1A2+b2A3+b3x2 (modelo completo)
+#con el nivel de referencia igual a A1
+#Además notar que en este caso,
+#si consideramos que x1c toma el valor del nivel A1,
+#entonces el modelo para E(Y;x1c=A1,x2) es
+# todos los parámetros son 0 
+#E(Y;x1c=A1,x2)=b0+b1(0)+b2(0)+b3x2 =b0+b3x2
+
+#para nivel A2: E(Y;x1c=A2,x2)
+#E(Y;x1c=A2,x2)=b0+b1(1)+b2(0)+b3x2=(b0+b1)+b3x2
+
+#para nivel A3: E(Y;x1c=A3,x2)
+#E(Y;x1c=A3,x2)=b0+b1(0)+b2(1)+b3x2=(b0+b2)+b3x2
+
+#Es decir, con este modelo se considera que la E(Y;x1c,x2)
+#es una recta en función del valor de x2 para cada nivel, 
+#con diferente intercepto pero misma pendiente.
+
+
+
+####### Otras formas de definir el mismo modelo en R
+####### cuestiones computacionales
+#Indicando las 2 v. dicotómicas (niveles) que deben entrar al modelo
+fitAlt=lm(y~I(X1c=="A2")+I(X1c=="A3")+X2, data=Datos)
+summary(fitAlt)
+
+#cambiando el nivel de referencia que tiene por default R
+#Aquí hay que obtener de nuevo las expresiones de
+#E(Y;x1c=A1,x2), E(Y;x1c=A2,x2) y E(Y|;x1c=A3,x2)
+fitAlt2=lm(y~relevel(X1c, "A3")+X2, data=Datos)
+summary(fitAlt2)
+
+fitAlt3=lm(y~I(X1c=="A1")+I(X1c=="A2")+X2, data=Datos)
+summary(fitAlt3)
+
+
+#el efecto de la función relevel
+levels(Datos$X1c)
+# se cambia la categoría de referencia y ahora será A3
+Datos$X1c=relevel(Datos$X1c, "A3")
+levels(Datos$X1c)
+
+#el efecto de la función factor con argumento levels
+Datos$X1c=factor(Datos$X1c, levels=c("A1","A2","A3"))
+# Levels ya define el orden de tus niveles 
+levels(Datos$X1c)
+
+
+#######
+####### Regresemos al modelo
+
+summary(fit)
+#Observemos que con la prueba asociada a la tabla ANOVA, 
+#se puede concluir que con una significancia de .05,
+#se rechaza H0 en el contraste
+#H0: b1=0 y b2=0 y b3=0 vs Ha: b1!=0 o b2!=0 o b3!=0
+
+#Además, si analizamos la prueba t, para el coeficiente b3, 
+#asociado a X2, podemos observar que se rechaza H0
+#en el contraste H0: b3=0 vs Ha: b3 != 0.
+#Esto nos indicaría que una vez incluida la variable 
+#categórica X1c en el modelo, la variable 
+#X2 está agregando información.
+# Una vez definida la var categórica en nuestro modelo, x2 sí está agregando información 
+
+#Para analizar si la variable X1c agrega información
+#dado que el modelo incluye a X2,
+#debemos constrastar 
+
+#Aquí E(y;x1c, x2)=b0+b1A2+b2A3+b3x2 (modelo completo)
+
+#H0: b1=0 y b2=0 vs Ha: b1!=0 o b2!=0
+
+#Opción 1, usando multcomp
+library(multcomp)
+K=matrix(c(0,1,0,0,
+           0,0,1,0), ncol=4, nrow=2, byrow=TRUE)
+m=c(0,0)
+summary(glht(fit, linfct=K, rhs=m), test=Ftest())
+
+'''
+	 General Linear Hypotheses
+
+Linear Hypotheses:
+       Estimate
+1 == 0     1.31
+2 == 0     0.50
+
+Global Test:
+     F DF1 DF2    Pr(>F)
+1 4442   2 296 1.76e-221
+'''
+#Se observa que se rechaza H0, 
+#entonces la variable X1c nos proporciona información para
+#modelar E(Y;x) aun cuando en el modelo ya está la variable X2.
+
+
+#Opción 2, usando anova con modelos reducidos
+fitred=lm(y~X2, data=Datos)
+anova(fitred, fit)
+
+#Opción 3, usar directamente la función drop1
+# ver si individualmente, al quitar las variables te da o no información sobre el modelo
+drop1(fit, test = "F")
+
+'''
+Single term deletions
+
+Model:
+y ~ X1c + X2
+       Df Sum of Sq  RSS   AIC F value Pr(>F)    
+<none>                 3 -1384                   
+X1c     2        87   90  -357    4442 <2e-16 *** # significa que incluir esta variable está mejorando el modelo
+X2      1      2371 2373   626  241851 <2e-16 ***
+---
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+'''
+
+#Opción 4, usar directamente la función Anova en librería car
+install.packages("car")
+library(car)
+Anova(fit, type="II")
+#Notar que lo anterior sólo se vale cuando 
+#la variable categórica aparece en el modelo sólo a través de sus 
+#k-1 v. dicotómicas (no hay interacciones entre variables).
+
+
+#Por otro lado, si analizamos la prueba t, para el coeficiente b1,
+#se rechaza H0. Aquí el contraste es H0: b1=0 vs Ha: b1 != 0.
+#Esto nos indica, que condicional en un valor fijo de X2,
+#la esperanza de Y es diferente entre 
+#el nivel A2 y el A1 (de referencia)
+
+
+
+#Ahora, si analizamos la prueba t, para el coeficiente b2,
+#se rechaza H0. Aqu? el contraste es H0: b2=0 vs Ha: b2 != 0.
+#Esto nos indica, que condicional en un valor fijo de X2,
+#la esperanza de Y es diferente entre
+#el nivel A3 y el A1 (de referencia)
+
+#Con base en lo anterior, 
+#parece que no podr?amos reducir el modelo, todos 
+#los coeficientes parecen significativos.
+
+#Interpretaci?n del modelo
+
+summary(fit)
+#R2, el coeficiente de determinaci?n, nos indica que se est?
+#explicando el 99.9% de la variabilidad observada en Y a trav?s 
+#del modelo que incluye ambas variables X1c y X2:
+#E(y;X1c, X2)=b0+b1A2+b2A3+b3x2
+
+#Adem?s, b1 se puede interpretar como:
+#condicionado en un valor fijo de X2, el promedio de la variable 
+#Y aumenta en 1.31 unidades al comparar el nivel A2 contra el A1.
+#(mayor esperanza en nivel A2 contra nivel A1)
+#Recordar que nivel A1 es el de referencia en este modelo.
+#Esta interpretaci?n se obtiene al comparar
+#E(Y;A2, X2)-E(Y;A1, X2)=b0+b1+b3x2-(b0+b3x2)=b1
+
+
+#b2 se puede interpretar como:
+#condicionado en un valor fijo de X2, el promedio de la variable 
+#Y aumenta en .5 unidades al comparar el nivel A3 contra el A1.
+
+#b3 se puede interpretar como
+#condicionado en un nivel fijos de la variable X1c, el promedio 
+#de la variable Y aumenta en 1.5 unidades 
+#al aumentar en una unidad la variable X2.
+
+#?C?mo se comparan los niveles A2 y A3, dado un valor fijo de X2?
+#E(Y;A2, X2)=bo+b1+b3X2
+#E(Y;A3, X2)=bo+b2+b3X2
+
+#E(Y;A3, X2)-E(Y;A2, X2)=b2-b1
+
+#Por ejemplo si se tiene inter?s en contrastar
+#H0:E(Y;A2, X2) =E(Y;A3, X2) vs
+#Ha:E(Y;A2, X2)!=E(Y;A3, X2)
+
+#Las hip?tesis se pueden escribir como
+#H0:b2-b1 =0 vs
+#Ha:b2-b1!=0
+
+#se puede usar multcomp
+
+library(multcomp)
+K=matrix(c(0,-1,1,0), ncol=4, nrow=1, byrow=TRUE)
+m=c(0)
+summary(glht(fit, linfct=K, rhs=m), test=Ftest())
+summary(glht(fit, linfct=K, rhs=m))
+
+#Se rechaza la igualdad de medias, es decir, dado un valor de X2, 
+#la esperanza de Y es diferente entre los niveles A3 y A2
+#De hecho, la esperanza disminuye en .8135 unidades
+#al comparar el nivel A3 contra el A2
+
+
+#Para visualizar resultados del modelo
+#E(y;X1c,X2)=b0+b1A2+b2A3+b3x2
+
+#Recordar que en este caso, el modelo
+#se relaciona con tres rectas, una por nivel
+#Nivel A1
+#E(Y;A1,x2)=b0+b3x2
+
+#Nivel A2
+#E(Y;A2,x2)=b0+b1+b3x2=(b0+b1)+b3x2
+
+#Nivel A3
+#E(Y;A3,x2)=b0+b2+b3x2=(b0+b2)+b3x2
+
+#Es decir como funci?n de X2, 
+#son tres rectas que tienen diferente intercepto
+#pero misma pendiente b3
+fit$coefficients
+#coef(fit)
+fA1 <- function(X2) {fit$coefficients[1]+ fit$coefficients[4]*X2}
+fA2 <- function(X2) {fit$coefficients[1]+fit$coefficients[2]+ fit$coefficients[4]*X2}
+fA3 <- function(X2) {fit$coefficients[1]+fit$coefficients[3]+ fit$coefficients[4]*X2}
+
+X11()
+with(Datos, plot(X2, y, col=c("red", "green", "blue")[Datos[,2]] ))
+legend("topleft",levels(Datos[,2]), col=c("red", "green", "blue"), pch = c(0,0,0), pt.cex=1.5,cex = .9, y.intersp = 1.4 , bty="n" )
+curve(fA1, from = min(Datos$X2), to = max(Datos$X2),
+      col = "red", add = T)
+curve(fA2, from = min(Datos$X2), to = max(Datos$X2),
+      col = "green", add = T)
+curve(fA3, from = min(Datos$X2), to = max(Datos$X2),
+      col = "blue", add = T)
+
---
+++ RLM_Ancova_Cadmium.R
@@ -0,0 +1,418 @@
+#Ejemplo ANCOVA
+#Exposición a cadmio
+
+# Fechas de grabación 
+
+# 2022 - 08 - 23
+# 2022 - 08 - 25
+#
+rm(list = ls(all.names = TRUE))
+gc()
+
+setwd("/home/diego/Documents/Aprendizaje_Estadistica")
+options(digits=4)  
+
+
+CADdata <- read.table("cadmium.txt",header=TRUE, sep=" ", dec=".")
+str(CADdata)
+
+#La variable grupo debe ser convertida a factor
+
+CADdata$group=factor(CADdata$group, levels=c(1,2,3), labels=c("High","Low","No") )
+str(CADdata)
+
+#Una gráfica global que representan pares de variables
+X11()
+#install.packages("GGally")
+library(GGally)
+ggpairs(data=CADdata, title="Datos", aes(colour = group))
+
+
+#Otras gráficas
+X11()
+par(mfrow=c(1,2)); par(mar=c(4,4,2,1.5))
+boxplot(vitcap ~ group, data = CADdata, col = "white", outline=FALSE)
+stripchart(vitcap ~ group, data = CADdata,
+           method = "jitter",
+           pch = 19,
+           col = 2:4,
+           vertical = TRUE,
+           add = TRUE)
+boxplot(age ~ group, data = CADdata, col = "white", outline=FALSE)
+stripchart(age ~ group, data = CADdata,
+           method = "jitter",
+           pch = 19,
+           col = 2:4,
+           vertical = TRUE,
+           add = TRUE)
+
+# Filtramos los datos por edad
+summary(CADdata[CADdata$group=="High",])
+CADdata=CADdata[CADdata$age>=39,]
+
+
+#La gráfica de interés
+X11()
+par(mfrow=c(1,2)); par(mar=c(4,4,2,1.5))
+
+plot(CADdata$age, CADdata$vitcap, col=c("green","red","blue")[CADdata$group],xlab = "Age", 
+     ylab = "Vital Capacity (L)",xlim=c(35,68), ylim=c(2.5,5.6), 
+     pch=c(16,16,16), main = "Exposure to cadmium")
+
+legend(60,5.8, levels(CADdata$group),
+       col=c("green","red","blue"), pch = c(16,16,16) )
+
+boxplot(vitcap ~ group, data = CADdata, col = "white", outline=FALSE)
+stripchart(vitcap ~ group, data = CADdata,
+           method = "jitter",
+           pch = 19,
+           col = c("green","red","blue"),
+           vertical = TRUE,
+           add = TRUE)
+
+#Importante analizar el metadato de niveles, pues
+#el primero será el nivel de referencia en los modelos
+
+levels(CADdata$group)
+View(CADdata)
+# Sólo tenemos categoría de group: high, low, no 
+
+#Aquí la referencia es High
+#Ajuste del modelo con interacciones con las otras variables explicativas (en este caso: edad)
+# E(y;x)= b0 + b1 age + b2 Low + b3 No + b4(age*Low) + b5(age*No)
+fit <- lm(vitcap ~ age * group, data = CADdata) 
+summary(fit)
+
+#E(Y;group=High, age)=  b0 + b1 age # como es la tercera categoría, todas las demás son 0 
+#E(Y;group= Low, age)=  b0 + b1 age +b2 + b4 age = (b0 + b2) + (b1 + b4) age
+#E(Y;group=  No, age)=  b0 + b1 age +b3 + b5 age = (b0 + b3) + (b1 + b5) age
+
+#Este modelo considera tres diferentes rectas, una para cada grupo
+
+#Se rechaza H0 en la prueba asociada
+#a la tabla ANOVA (p-value: 0.000592), 
+#entonces el modelo tiene sentido.
+#Es decir, la edad o el nivel de exposición ayudan a modelar E(Y)
+
+#Seguimos con prueba de igualdad de pendientes 
+#(coeficientes asociados a las interacciones)
+#Si no se rechaza, podríamos optar por un modelo con igualdad de 
+#pendientes o rectas paralelas (facilita la interpretación)
+
+# Prueba 
+#H0: b4=0 y b5=0 vs Ha: b4!=0 o b5!=0
+
+library(multcomp)
+#        b1 b2 b3 b4 b5
+K=matrix(c(0,0,0,0,1,0,          # Se van a contrastar dos variables 
+           0,0,0,0,0,1), ncol=6, nrow=2, byrow=TRUE)
+m=c(0,0)
+summary(glht(fit, linfct=K, rhs=m), test=Ftest())
+
+# Se rechaza H0 (p-value: 0.0091). No se puede considerar el 
+# modelo con rectas paralelas pues rechazamos b4 = 0, b5 = 0 
+
+# Procedemos a ver si las tres rectas tienen pendiente diferente
+
+# Prueba simultánea 
+
+# Es decir, es la prueba simultánea que sigue a la prueba lineal general
+
+#Notar que aquí se incluyen
+#las tres hipótesis individuales asociadas a
+
+# H0_1: pendientes de High y Low iguales (b4=0) (parece que tienen una misma pendiente una vez que no se rechaza H0)
+# H0_2: pendientes de High y No  iguales (b5=0)
+# H0_3: pendientes de Low y No   iguales (b4-b5=0)
+
+#Esta prueba sólo detecta las diferencias más evidentes, 
+#la lectura se debe hacer con cuidado considerando
+#que esta prueba tiene más chance de cometer el error
+#tipo II (aceptar la hipótesis nula y que ésta sea falsa) esto es para reducir el modelo 
+
+#Todas las pendientes difieren
+
+# b1 b2 b3 b4 b5
+# b4 = 0 
+# b5 = 0 
+# b4 - b5 = 0 
+K=matrix(c(0,0,0,0,1,0,
+           0,0,0,0,0,1,           # porque son 3 combinaciones lineales 
+           0,0,0,0,1,-1), ncol=6, nrow=3, byrow=TRUE)
+m=c(0,0,0)
+#                           Como no se agrega la prueba F, se hará las pruebas automáticas 
+summary(glht(fit, linfct=K, rhs=m))
+
+#De forma simultánea podemos identificar que
+#Se rechaza H0 por dos diferencias
+#1. Se rechaza b5=0, pues p-value < 0.026. 
+
+#Hay evidencia sobre pendientes de High y No diferentes
+# Esto es porque aceptamos b5 != 0 y b4 = 0, es decir, diferentes 
+
+#2. Se rechaza b4-b5=0, pues p-value < 0.046. 
+#Hay evidencia sobre pendientes de Low y No diferentes
+
+#Con esta prueba no se rechaza b4=0, por lo que podríamos optar 
+#por considerar un modelo con b4=0.
+# significa que no se va a requerir una interacción entre exposición baja y edad 
+# De tener el siguiente modelo: E(y;x)= b0 + b1 age + b2 Low + b3 No + b4(age*Low) + b5(age*No)
+# Pasamos a tener: E(y;x)= b0 + b1 age + b2 Low + b3 No + b5(age*No)
+
+#Ajustemos el modelo reducido que sólo considera a (age*No)
+#Este nuevo modelo corresponde a
+
+# E(y;x)= b0 + b1 age + b2 Low + b3 No + b4(age*No) (son nuevas b's, no las del modelo original)
+
+fitred <- lm(vitcap ~ age + group + I(age*(group=="No")), data = CADdata) 
+# aquí vemos pruebas t individuales 
+summary(fitred)
+
+# group = High -> E(y;x)= b0 + b1 age
+# group = Low -> E(y;x)= b0 + b1 age + b2 
+# group = No -> E(y;x)= b0 + b1 age + b3 + b4 age
+
+#E(Y;group=High, age)=  b0 + b1 age
+#E(Y;group= Low, age)=  b0 + b1 age +b2 = (b0 + b2) + b1 age
+#E(Y;group=  No, age)=  b0 + b1 age +b3 + b4 age = (b0 + b3) + (b1 + b4) age
+
+#Se rechaza H0 en la prueba asociada
+#a la tabla ANOVA (p-value: 0.000245)
+
+# Ahora podríamos proceder a reducir un poco más el modelo
+# para tratar de facilitar la interpretación
+# o bien a contestar las preguntas sobre los investigadores
+
+# De la salida de fitted, se puede observar
+# que una opción es considerar b2=0
+
+#El modelo reducido quedaría como
+# E(y;x)= b0 + b1 age  + b2 No + b3(age*No)
+
+#E(Y;group=High, age)=  b0 + b1 age
+#E(Y;group= Low, age)=  b0 + b1 age 
+#E(Y;group=  No, age)=  b0 + b1 age +b2 + b3 age = (b0 + b2) + (b1 + b3) age
+
+# Con esto se tendrían rectas iguales para High y Low
+# por otro lado, se tiene otra recta para No
+
+fitred2 <- lm(vitcap ~ age + I(group=="No") + I(age*(group=="No")), data = CADdata) 
+summary(fitred2)
+
+#Se rechaza H0 en la prueba asociada
+#a la tabla ANOVA (p-value: 7.26e-05)
+#También se puede observar que no se podría reducir más el modelo
+
+#Las rectas ajustadas son
+
+#\hat{E}(Y;group=High, age)=\hat{E}(Y;group=Low, age)= 8.4499 -0.0910 age
+#\hat{E}(Y;group=No, age)= (8.4499-3.6605)+(-0.0910+0.0781) age =4.789-0.0129 age
+
+#Se puede observar que la pendiente de los expuestos a cadmio 
+# es mayor en valor absoluto y negativa  
+# con lo que podemos interpretar que la capacidad vital
+# decrece más rápido en ese grupo comparado con los no expuesto.
+
+#Esto se puede observar más fácilmente en una gráfica
+
+
+#Modelo fitred2
+
+fitredHyL <- function(X2) {fitred2$coefficients[1]+ fitred2$coefficients[2]*X2}
+fitredN <- function(X2) {fitred2$coefficients[1]+fitred2$coefficients[3]+ (fitred2$coefficients[2]+fitred2$coefficients[4])*X2}
+
+X11()
+with(CADdata, plot(age, vitcap, col=c("red", "green", "blue")[CADdata[,1]] , pch = c(16,16,16)))
+legend("topright",levels(CADdata[,1]), col=c("red", "green", "blue"), pch = c(16,16,16), pt.cex=1.5,cex = .9, y.intersp = 1.4 , bty="n" )
+
+curve(fitredHyL, from = min(CADdata$age), to = max(CADdata$age),
+      col = "red", add = T)
+curve(fitredN, from = min(CADdata$age), to = max(CADdata$age),
+      col = "blue", add = T)
+
+
+####
+#Una revisi?n r?pida de los supuestos
+X11()
+par(mfrow = c(2,2), mgp = c(2,0.7,0), mar = c(3,3,1.5,1))
+plot(fitred2, 1)   #linealidad
+plot(fitred2, 3)   #homocedasticidad
+plot(fitred2, 2)   #normalidad
+plot(fitred2, 5, cook.levels = c(4/(dim(CADdata)[1]-2), 0.5, 1.0))   #Outliers 
+
+#Homocedasticidad
+library(car)
+#H0: la varianza es constante 
+car::ncvTest(fitred2)
+#No se rechaza H0, no hay evidencia en contra de la homocedasticidad
+
+#Normalidad 
+#Se basa en los residuales estandarizados o estudentizados
+#H0: los datos provienen de una distribuci?n normal
+library(broom)
+Datosfitred2=augment(fitred2)
+shapiro.test(Datosfitred2$.std.resid)
+library(normtest)
+normtest::jb.norm.test(Datosfitred2$.std.resid)
+#No se rechaza H0, no hay evidencia en contra de la normalidad
+
+#Linealidad
+X11()
+library(car)
+residualPlots(fit)
+
+
+### Uso del modelo
+# Con este modelo se pueden contestar preguntas puntuales,
+# por ejemplo, si a los 50 a?os ya se nota en promedio
+# una diferencia entre los que est?n expuestos a cadmio y los que no
+
+#H0: E(Y;group= High(Low), age=50)=E(Y;group=  No, age=50)
+#vs
+#Ha: E(Y;group= High(Low), age=50)!=E(Y;group=  No, age=50)
+
+#Notar que estas hip?tesis se pueden escribir en t?rminos de 
+#los par?metros:
+
+#H0: b0 + b1 50 = (b0 + b2) + (b1 + b3) 50
+#Se pasan los par?metros de un lado de la igualdad 
+#H0: b2+b3 (50) = 0
+
+K=matrix(c(0,0,1,50), ncol=4, nrow=1, byrow=TRUE)
+m=c(0)
+summary(glht(fitred2, linfct=K, rhs=m))
+
+#No se rechaza H0, es decir, a los 50 a?os no se encuentra evidencia
+#que indique una diferencia promedio en la capacidad vital
+#entre los que fueron expuestos y los que no
+
+
+#A veces es importante incluir la direcci?n
+#es decir, los investigadores sospechan que 
+#a los 55 a?os la capacidad vital es en promedio
+#mayor en los no expuestos comparados con los expuestos
+
+#Aqu? tomamos informaci?n del contexto del problema
+#con base en lo que sospechan los investigadores
+#esto se expresa con una direcci?n en la forma de
+#plantear las hip?tesis
+
+#H0: E(Y;group= High(Low), age=55) >= E(Y;group=  No, age=55)
+#vs
+#Ha: E(Y;group= High(Low), age=55)  < E(Y;group=  No, age=55)
+
+#Notar que en la Ha s?lo se permiten los casos !=, < o >
+
+#Esto se puede escribir en t?rminos de los par?metros
+#H0: b0 + b1 55 >= (b0 + b2) + (b1 + b3) 55
+
+#Es decir, pasando los par?metros a un s?lo lado de la 
+#desigualdad
+#H0: 0 >= b2 + b3 55
+#vs
+#Ha: 0 < b2 + b3 55   
+
+#La alternativa se usa para definir la direcci?n
+# < f(b0,b1,...,bp) es greater
+# > f(b0,b1,...,bp) es less
+
+K=matrix(c(0,0,1,55), ncol=4, nrow=1, byrow=TRUE)
+m=c(0)
+summary(glht(fitred2, linfct=K, rhs=m, alternative="greater"))
+
+#Se rechaza H0, es decir,
+#hay evidencia para concluir que el promedio de capacidad vital
+#es menor en los expuestos comparados con los no expuestos
+#a la edad de 55 a?os.
+#La diferencia promedio estimada es .635
+
+
+#Una forma de resumir los resultados es presentar una gr?fica 
+#con los intervalos de confianza simult?neos. Con esto
+#es posible identificar en que punto ya se podr?a observar 
+#una diferencia entre los promedios en la capacidad vital
+
+
+
+#Hay dos opciones
+#I. La m?s f?cil de interpretar corresponde a incluir los datos
+#las rectas ajustadas y los intervalos de confianza de E(Y).
+#Dado que no considera ninguna informaci?n adicional (direcci?n)
+#podr?a evitar distinguir claramente algo
+
+X11()
+par(mfrow = c(1,2))
+with(CADdata, plot(age, vitcap, col=c("red", "green", "blue")[CADdata[,1]] , pch = c(16,16,16)))
+legend("topright",levels(CADdata[,1]), col=c("red", "green", "blue"), pch = c(16,16,16), pt.cex=1.5,cex = .9, y.intersp = 1.4 , bty="n" )
+
+summary(CADdata)
+
+#Creamos una malla de valores asociados a la edad
+age <- seq(from = 39, to = 65, by = .5)
+length(age)
+
+#Calculares los intervalos de confianza simult?neos para la esperanza
+#de la capacidad vital, para expuestos y no expuestos
+#Bajar? la confianza a 90%, pues ser?n intervalos simult?neos
+#de la esperanza y estos suelen ser muy conservadores
+
+#Para la banda del grupo expuesto (high o low)
+#E(Y;group=High (low), age)= b0 + b1 age
+KH <- cbind(1, age, 0, 0)
+(KH)
+#Para del grupo no expuesto
+#E(Y;group=No, age)= (b0 + b2) + (b1 + b3) age
+KN <- cbind(1, age, 1,age)
+(KN)
+
+K=rbind(KH, KN)
+
+fitE <- glht(fitred2, linfct = K)
+fitci <- confint(fitE, level = 0.90)
+
+lines(age, coef(fitE)[1:53], col="red")
+lines(age, fitci$confint[1:53,"upr"], col="red")
+lines(age, fitci$confint[1:53,"lwr"], col="red")
+
+lines(age, coef(fitE)[54:106], col="blue")
+lines(age, fitci$confint[54:106,"upr"], col="blue")
+lines(age, fitci$confint[54:106,"lwr"], col="blue")
+
+
+#Estas bandas de confianza o intervalos de confianza simult?neos sirven para identificar
+#las diferencias m?s evidentes con los datos, pero son muy conservadoras
+
+
+#II. Un poco m?s dif?cil de interpretar y corresponde
+#a incluir el intervalo de confianza directamente
+#de la diferencia de esperanzas para una edad fija entre
+#expuestos y no expuestos, es decir
+
+#E(Y;group=High (low), age)-E(Y;group=No, age)
+#= [b0 + b1 age] - [(b0 + b2) + (b1 + b3) age]
+#= -b2-b3 age
+
+#Notar que ahora nos interesan aquellos valores 
+#cuyos intervalos est?n por abajo de 0
+#pues con eso conluiremos que para esas edades 
+#se observan en promedio menor capacidad vital en los
+#expuestos
+
+
+#Creamos una malla de valores asociados a la edad
+age <- seq(from = 39, to = 65, by = .5)
+length(age)
+K <- cbind(0, 0, -1,-age)
+
+fitE <- glht(fitred2, linfct = K)
+fitci <- confint(fitE, level = 0.90)
+
+plot(age, coef(fitE), col="black", type="l", main="Diferencia de E(vitcap) entre expuestos y no", ylab="E(Y;High-Low) - E(Y;No)")
+lines(age, fitci$confint[,"upr"], col="black")
+lines(age, fitci$confint[,"lwr"], col="black")
+abline(h=0, col="blue")
+
+#A partir de los 54 a?os se observa que 
+#la esperanza de la capacidad vital es menor 
+#en las personas expuestas a cadmio en comparaci?n
+#con las no expuestas. Esta diferencia aumenta con la edad
